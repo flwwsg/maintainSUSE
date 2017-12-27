7e6ff7ceb71d3412003188a720018aa4f6e5a99e
flwwsg <2319406132@qq.com>
2017-12-27 23:55:28 +0800

    bugfix

---
+++ custom.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python3
+"""
+Changing Repository from dowload.opensuse.org to mirrors.tuna.tsinghua.edu.cn/opensuse and 
+install popular software.
+Running only once on first running after system installed
+"""
+import os
+import sys
+import time
+import configparser
+from urllib.request import urlopen
+import json
+import pwd
+from socket import timeout
+
+# SUPPORTEDOS = ['opensuse', 'tumbleweed']
+# path = os.path.abspath(__file__)
+# fpath = os.path.join(os.path.dirname(path), 'configs.json')
+# CONFIGS = json.load(open(fpath))
+
+# def get_userinfo(uid=1000):
+#     try:
+#         infos = pwd.getpwuid(uid)
+#     except Exception:
+#         return None, None
+
+#     dirname = infos.pw_dir
+#     username = infos.pw_name
+#     return username, dirname
+
+
+# config = get_config()
+# SOFTS = {'opensuse': ('basic_suse_softs', 'suse_softs')}
+
+
+# def changerepo(plantform='opensuse', mirrorname='tuna'):
+#     """change software repository to mirror site in china"""
+#     mirrorurl = config['common'][mirrorname] + plantform
+#     repos = config[plantform]['repos'].split()
+#     reserved = config[plantform].get('reserved', '').split()
+#     default_url = 'http://download.opensuse.org'
+#     addrepo = 'sudo zypper addrepo --check --refresh --name "%s" %s "%s"'
+#     removerepo = 'sudo zypper removerepo %s'
+
+#     outs = os.popen('zypper repos -d').readlines()
+#     newrepos = {}
+#     for line in outs:
+#         tmp = line.split('|')
+#         if len(tmp) < 8 or tmp[1].strip() == 'Alias':
+#             continue
+
+#         alias = tmp[1].strip()
+#         if alias not in reserved:
+#             os.system(removerepo % alias)
+#         else:
+#             continue
+#         if alias not in repos:
+#             continue
+#         url = tmp[8]
+#         turl = url.replace(default_url, mirrorurl)
+#         tname = alias if alias.startswith(
+#             mirrorname + '-') else mirrorname + '-' + alias
+#         newrepos[tname] = turl
+
+#     for name, url in newrepos.items():
+#         os.system(addrepo % (name, url, name))
+#     os.system('sudo  zypper update ')
+
+
+# def install_software(plantform='opensuse', softs=[]):
+#     if not softs:
+#         tlist = SOFTS.get(plantform, [])
+#         for ll in tlist:
+#             softs.extend(config['common'].get(ll, []).split(',')[:-1])
+#     for soft in softs:
+#         os.system('sudo zypper in -y %s' % soft.strip())
+
+
+# def install_pip_module(file='', softs=[]):
+#     pipindex = config['common'].get(
+#         'pipindex', 'https://pypi.python.org/simple').strip()
+#     if not softs:
+#         softs = config['common'].get('pip_softs', []).split(',')[:-1]
+        
+#     if not file:
+#         os.system(
+#             'sudo pip install -r requirements.txt -i %s' % pipindex)
+#     else:
+#         os.system(
+#             'sudo pip install -r %s -i %s' % (file, pipindex))
+
+#     for soft in softs:
+#         os.system(
+#             'sudo pip install -i %s %s' % (pipindex, soft))
+
+
+# def improved_bash(alias={}, echos=[], cmds=[], filename=''):
+#     username, userdir = get_userinfo()
+#     if not filename:
+#         filename = userdir+'/.bashrc'
+
+#     if not alias:
+#         newalias = config['bash']['alias'].split(',')[:-1]
+#         for tmp in newalias:
+#             tmp = tmp.strip()
+#             func = tmp.strip().split('=')[0]
+#             alias[func] = tmp[len(func)+1:]
+
+#     if not echos:
+#         echos = config['bash']['echos'].split(',')[:-1]
+
+#     if not cmds:
+#         cmds = config['bash']['cmds'].split(',')[:-1]
+        
+#     for cmd, alia in alias.items():
+#         os.system('echo "alias %s=%s" >> %s' % (cmd, alia, filename))
+
+#     for cmd in echos:
+#         os.system('echo \'%s >> %s\'' % (cmd.strip(), filename))
+
+#     for cmd in cmds:
+#         os.system(cmd)
+
+
+# def add_repos(repos='', plantform='opensuse', version='42.2'):
+#     if config[plantform]['version'] != version:
+#         return False
+#     if not repos:
+#         repos = config[plantform]['addrepos'].split(',')[:-1]
+#     for repo in repos:
+#         os.system('sudo zypper ar -fc %s' % repo.strip())
+#     repos = config['common']['cusrepos'].split(',')[:-1]
+#     for repo in repos:
+#         # print(repo.lstrip())
+#         os.system(repo.lstrip())
+#     os.system('sudo zypper  --gpg-auto-import-keys ref')
+#     return True
+
+
+# def add_group():
+#     groups = config['common']['group'].split(',')[:-1]
+#     username, userdir = get_userinfo()
+#     for g in groups:
+#         os.system(g.strip() % username)
+
+class CustomOS(object):
+    """change repository to chinese mirror"""
+
+    def __init__(self, mirror_name, file_name='configs.json'):
+        self.mirror_name = mirror_name
+        self.config_file = file_name
+        self.plantform = ''
+        self.version = ''
+        # self._chk_permission()
+        # self._get_config()
+        # self._get_plantform()
+
+    def _get_plantform(self):
+        with open('/etc/os-release') as f:
+            infos = f.readlines()
+        for line in infos:
+            tmp = line.strip().split('=')
+            name = tmp[0]
+            if name == 'ID':
+                self.plantform = tmp[1].replace('"','')
+            elif name == 'VERSION':
+                self.version = tmp[1].replace('"', '')
+        if not self.plantform or not self.version:
+            raise Exception('Unknown os.')
+
+
+    def _chk_permission(self):
+        euid = os.geteuid()
+        if euid != 0:
+            print('try: sudo python3 custom.py')
+            exit(1)
+
+    def _get_config(self):
+        try:
+            self.configs = json.load(open(self.config_file))
+        except Exception:
+            print('Can not read json file named %s, properly wrong json formate' % self.config_file)
+            sys.exit(1)
+    
+    def check_prerequirements(self):
+        '''
+        check requirement to run this program
+        '''
+        self._chk_permission()
+        self._get_config()
+        self._get_plantform()
+        self._chk_config()
+
+    def _chk_config(self):
+        '''
+        checking parameters specified in configs
+        '''
+
+        # checking repository
+        if self.plantform not in self.configs:
+            raise Exception('Not supported os named %s' % self.plantform)
+        if self.version not in self.configs[self.plantform]['os']:
+            raise Exception('Not supported version %s of %s' % (self.version, self.plantform))
+        
+        if not all([k in self.configs for k in ['common', 'bash']]):
+            raise Exception('Can not find common or bash parameters in file named %s' % self.config_file)
+
+        common_item = ['pypi', 'host', 'pip_software', self.mirror_name]]
+        if not all([k in self.configs['common'] for k in ['pypi', 'host', 'pip_software', self.mirror_name]]):
+            raise Exception('Can not find all "%s" in common in file named %s' % (common_item, self.config_file))
+        # checking pypi
+        if 'pypi' not in self.configs['common']:
+            raise Exception('Can not find pypi item in common dictory') 
+        for url in self.configs['common']['pypi']:
+            try:
+                resp = urlopen(url, timeout=20).read()
+            except timeout:
+                raise Exception('Can not access %s' % url)     
+            
+    def add_repo(self):
+        '''
+        add repository
+        '''
+        repos = self.configs[self.plantform]['os'][self.version]['repos']
+        custom_repos = self.configs[self.plantform]['custom_repos']
+        addrepo = 'sudo zypper addrepo --check --refresh --name "%s" %s "%s"'
+        for repo in repos:
+            os.system(addrepo % (repo['name'], repo['url'], repo['name']))
+        for crepo in custom_repos:
+            os.system(crepo)
+
+    def get_hosts(self):
+        '''
+        get host file to fight GFW
+        '''
+        html = urlopen(self.configs['common']['host'], timeout=30)
+        with open('hosts', 'wb') as f:
+            f.write(html.read())
+        # copy hosts
+        os.system('sudo cat ./hosts >> /etc/hosts')
+        os.system('sudo systemctl restart NetworkManager')
+        time.sleep(10)
+
+    def install_software(self):
+        '''
+        install software
+        '''
+
+
+
+# # #configure git
+# # os.system('git config --global user.email "2319406132@qq.com"')
+# # os.system("git config --global user.name 'flwwsg'")
+
+# # # add groups
+# # for group in groups:
+# # 	os.system('sudo usermod -aG %s lblue' % group)
+# # 	os.system('sudo usermod -aG %s dev' % group)
+# # # sudo usermod -aG groupName userName
+# # # sudo usermod -aG vboxusers lblue
+
+# if __name__ == '__main__':
+#     def testing():
+#         print('ok')
+
+#     flist = [
+#         changerepo,
+#         install_software,
+#         get_hosts,
+#         install_pip_module,
+#         improved_bash,
+#         add_repos,
+#         add_group,
+#         testing,
+#     ]
+#     if len(sys.argv) < 2:
+#         print('using python3 changeRepo.py function_you_want_to_run')
+#         for i, func in enumerate(flist):
+#             print('for %s enter: %s\n' % (func.__name__, i + 1))
+#     else:
+#         for num in sys.argv:
+#             try:
+#             # print(sys.argv)
+#             	flist[int(num) - 1]()
+#             except Exception:
+#                 pass
--- tests.py
+++ tests.py
@@ -1,160 +1,158 @@
-# import os
-
-# OSINFO = {'id':'plantform', 'pretty_name':'version'}
-# SUPPORTEDOS = ['opensuse']
-# class ChangeRepo(object):
-# 	"""change repository to chinese mirror"""
-# 	def __init__(self, plantform=''):
-# 		if not plantform:
-# 			versions = os.popen('cat /etc/os-release').readlines()
-# 			for line in versions:
-# 				tmp = line.strip().replace('"','').split('=')
-# 				if tmp[0].lower() in OSINFO:
-# 					setattr(self, plantform, tmp[1])
-
 import os
 import sys
-import changeRepo as cr
+from custom import CustomOS
 import unittest
 from unittest import mock
+import json
 
 
-_supported = ['supported system', 'change repositories',
-              'install pop software', 'install basic software', 'adding imporved bashrc', 'install pip module']
+# _supported = ['supported system', 'change repositories',
+#               'install pop software', 'install basic software', 'adding imporved bashrc', 'install pip module']
 
-
-class BaseChangeRepoTest(unittest.TestCase):
-    """basic tests set for changeRepo.py"""
-
-    class ChangeRepo(cr.ChangeRepo):
-        def _chk_permission(self):
-            pass
-
-    # def setUp(self):
-    #     pass
-
-
-class TestChangeRepo(BaseChangeRepoTest):
-    def test_supported_system(self):
-        self.assertIn('opensuse', cr.SUPPORTEDOS)
-
+class TestInitialzeOpensuse(unittest.TestCase):
+    
+    def setUp(self):
+        self.configs = json.load(open('configs_test.json'))
+        mock_chk_permission = mock.MagicMock(return_value=True)
+        with mock.patch('custom.CustomOS._chk_permission', mock_chk_permission):
+            self.cr = CustomOS('ustc', 'configs_test.json')
+    
     def test_change_repo(self):
-        reposurl = []
-        plantform = 'tumbleweed'
-        # mirror = 'https://mirrors.tuna.tsinghua.edu.cn/'+plantform
-        mirror = 'https://mirrors.ustc.edu.cn/' + plantform
-        with mock.patch('os.system', lambda x: reposurl.append(x)):
-            cr.changerepo(plantform=plantform, mirrorname='ustc')
-        self.assertTrue(reposurl)
-        self.assertTrue(
-            all([mirror in url for url in reposurl if url.startswith('sudo zypper addrepo')]))
-
-    # @unittest.skip
-    def test_install_software(self):
         infos = []
-        softs = ['imagewriter']
         with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.install_software(plantform='opensuse', softs=softs)
-        softs1 = [ '-t pattern devel_basis', 'chromium', 'git', 'fcitx-table-cn-wubi-pinyin', 'ctags',]
-        softs.extend(softs1)
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.install_software(plantform='opensuse')
-        patts = ['sudo zypper in -y %s' % soft for soft in softs]
+            self.cr.add_repo()
         self.assertTrue(infos)
-        self.assertEqual(infos, patts)
+        self.assertTrue(all([url.startswith('sudo zypper addrepo') for url in infos]))
 
-    # @unittest.skip
-    def test_install_pip_module(self):
-        patts = ['sudo pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/',
-                 'sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ selenium',
-                 'sudo pip install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/',
-                 'sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ virtualenvwrapper'
-                 ]
-        infos = []
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.install_pip_module(file='requirements.txt', softs=['selenium'])
-
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.install_pip_module(file='requirement.txt')
-        
-        self.assertTrue(infos)
-        self.assertEqual(infos, patts)
-
-    # @unittest.skip
-    def test_improved_bash(self):
-        infos = []
-        patts = ['echo "alias grep=\'grep -E --color=auto\'" >> /home/dev/.bashrc',
-                 'echo PATH=~/.local/bin:$PATH >> /home/dev/.bashrc',
-                 'echo export PATH=~/bin:$PATH >> /home/dev/.bashrc']
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.improved_bash()
-
-        self.assertTrue(infos)
-        self.assertEqual(infos, patts)
-
-    @unittest.skip
-    def test_update_hosts(self):
-        infos = []
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.get_hosts()
+    def test_get_hosts(self):
+        if os.path.exists('hosts'):
+            os.remove('hosts')
+        with mock.patch('os.system', lambda x: print(x)):
+            self.cr.get_hosts()
         self.assertTrue(os.path.exists('hosts'))
-        self.assertTrue(os.path.exists('hosts_test'))
-        hosts = open('hosts').read()
-        hosts_test = open('hosts_test').read()
-        self.assertTrue(infos)
-        self.assertEqual(hosts, hosts_test)
+        self.assertTrue(open('hosts').read())
 
-    def test_get_user_info(self):
-        infos = cr.get_userinfo(1100)
-        self.assertEqual(infos,(None,None))
-        infos = cr.get_userinfo()
-        self.assertEqual(infos, ('dev', '/home/dev'))
+# class TestChangeRepo(BaseChangeRepoTest):
+#     def test_supported_system(self):
+#         self.assertIn('opensuse', cr.SUPPORTEDOS)
 
-    def test_add_repos(self):
-        infos = []
-        patts = [
-            'sudo zypper ar -fc https://mirrors.tuna.tsinghua.edu.cn/packman/suse/openSUSE_Leap_42.2/ tuna-packman',
-            'sudo zypper ar -fc http://download.opensuse.org/repositories/devel:/languages:/python3/openSUSE_Leap_42.2/ dev-py3',
-            'sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg',
-            'sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc',
-            'sudo zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo',
-            'sudo sh -c \'echo -e "[code]\\nname=Visual Studio Code\\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\\nenabled=1\\ntype=rpm-md\\ngpgcheck=1\\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo\''
-        ]
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            notnone = cr.add_repos()
-        self.assertTrue(notnone)
-        self.assertEqual(infos, patts)
+#     def test_change_repo(self):
+#         reposurl = []
+#         plantform = 'tumbleweed'
+#         # mirror = 'https://mirrors.tuna.tsinghua.edu.cn/'+plantform
+#         mirror = 'https://mirrors.ustc.edu.cn/' + plantform
+#         with mock.patch('os.system', lambda x: reposurl.append(x)):
+#             cr.changerepo(plantform=plantform, mirrorname='ustc')
+#         self.assertTrue(reposurl)
+#         self.assertTrue(
+#             all([mirror in url for url in reposurl if url.startswith('sudo zypper addrepo')]))
 
-    def test_plantform(self):
-        instance = self.ChangeRepo()
-        self.assertEqual(instance.plantform, 'opensuse')
-        self.assertEqual(instance.version, '42.2')
+#     # @unittest.skip
+#     def test_install_software(self):
+#         infos = []
+#         softs = ['imagewriter']
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.install_software(plantform='opensuse', softs=softs)
+#         softs1 = [ '-t pattern devel_basis', 'chromium', 'git', 'fcitx-table-cn-wubi-pinyin', 'ctags',]
+#         softs.extend(softs1)
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.install_software(plantform='opensuse')
+#         patts = ['sudo zypper in -y %s' % soft for soft in softs]
+#         self.assertTrue(infos)
+#         self.assertEqual(infos, patts)
 
-    def test_add_group(self):
-        infos = []
-        patts = [
-            'sudo usermod -aG vboxusers dev',
-            'sudo usermod -aG docker dev'
-        ]
-        with mock.patch('os.system', lambda x: infos.append(x)):
-            cr.add_group()
+#     # @unittest.skip
+#     def test_install_pip_module(self):
+#         patts = ['sudo pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/',
+#                  'sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ selenium',
+#                  'sudo pip install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/',
+#                  'sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ virtualenvwrapper'
+#                  ]
+#         infos = []
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.install_pip_module(file='requirements.txt', softs=['selenium'])
+
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.install_pip_module(file='requirement.txt')
         
-        self.assertTrue(infos)
-        self.assertEqual(infos, patts)
+#         self.assertTrue(infos)
+#         self.assertEqual(infos, patts)
+
+#     # @unittest.skip
+#     def test_improved_bash(self):
+#         infos = []
+#         patts = ['echo "alias grep=\'grep -E --color=auto\'" >> /home/dev/.bashrc',
+#                  'echo PATH=~/.local/bin:$PATH >> /home/dev/.bashrc',
+#                  'echo export PATH=~/bin:$PATH >> /home/dev/.bashrc']
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.improved_bash()
+
+#         self.assertTrue(infos)
+#         self.assertEqual(infos, patts)
+
+#     @unittest.skip
+#     def test_update_hosts(self):
+#         infos = []
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.get_hosts()
+#         self.assertTrue(os.path.exists('hosts'))
+#         self.assertTrue(os.path.exists('hosts_test'))
+#         hosts = open('hosts').read()
+#         hosts_test = open('hosts_test').read()
+#         self.assertTrue(infos)
+#         self.assertEqual(hosts, hosts_test)
+
+#     def test_get_user_info(self):
+#         infos = cr.get_userinfo(1100)
+#         self.assertEqual(infos,(None,None))
+#         infos = cr.get_userinfo()
+#         self.assertEqual(infos, ('dev', '/home/dev'))
+
+#     def test_add_repos(self):
+#         infos = []
+#         patts = [
+#             'sudo zypper ar -fc https://mirrors.tuna.tsinghua.edu.cn/packman/suse/openSUSE_Leap_42.2/ tuna-packman',
+#             'sudo zypper ar -fc http://download.opensuse.org/repositories/devel:/languages:/python3/openSUSE_Leap_42.2/ dev-py3',
+#             'sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg',
+#             'sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc',
+#             'sudo zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo',
+#             'sudo sh -c \'echo -e "[code]\\nname=Visual Studio Code\\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\\nenabled=1\\ntype=rpm-md\\ngpgcheck=1\\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo\''
+#         ]
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             notnone = cr.add_repos()
+#         self.assertTrue(notnone)
+#         self.assertEqual(infos, patts)
+
+#     def test_plantform(self):
+#         instance = self.ChangeRepo()
+#         self.assertEqual(instance.plantform, 'opensuse')
+#         self.assertEqual(instance.version, '42.2')
+
+#     def test_add_group(self):
+#         infos = []
+#         patts = [
+#             'sudo usermod -aG vboxusers dev',
+#             'sudo usermod -aG docker dev'
+#         ]
+#         with mock.patch('os.system', lambda x: infos.append(x)):
+#             cr.add_group()
+        
+#         self.assertTrue(infos)
+#         self.assertEqual(infos, patts)
         
 
-# if __name__ == '__main__':
-#     print('starting test....')
-    # help text
-# 	print('enter you want to test:')
+# # if __name__ == '__main__':
+# #     print('starting test....')
+#     # help text
+# # 	print('enter you want to test:')
 
-# 	maxlen = 0
-# 	for l in _supported:
-# 		if maxlen < len(l):
-# 			maxlen = len(l)
-# 	for i, item in enumerate(_supported):
-# 		print(item,' '*(maxlen-len(item)), 'enter:', i)
-# 	print('if you want to test all set ,enter "all". \
-# eg: I want to test "supported system and change repositories", so I enter "1,2"'
-# 		)
-# 	testset = input(':')
+# # 	maxlen = 0
+# # 	for l in _supported:
+# # 		if maxlen < len(l):
+# # 			maxlen = len(l)
+# # 	for i, item in enumerate(_supported):
+# # 		print(item,' '*(maxlen-len(item)), 'enter:', i)
+# # 	print('if you want to test all set ,enter "all". \
+# # eg: I want to test "supported system and change repositories", so I enter "1,2"'
+# # 		)
+# # 	testset = input(':')
